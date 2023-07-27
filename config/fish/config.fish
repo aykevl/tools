c269052393bdd329c472e1510c44d1fcf0e49cef
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-07-27 21:54:28 +0200

    Add exa (replacement for ls)
    Also sort and organize the aliases a bit.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,19 @@
 fish_vi_key_bindings
 set TERM "xterm-256color"
 set fish_greeting
-alias vim="nvim -p"
+
+# Misc aliases
 alias diff="diff --color=auto"
 alias df="df -h"
-alias free="free -h"
+alias ls="exa"
+alias ll="exa -l"
 alias egrep="egrep --color=auto"
-alias avr-objdump="avr-objdump --no-show-raw-insn"
+alias free="free -h"
 alias py=python3
+alias vim="nvim -p"
 
-# LLVM aliases
+# Compiler/binutils aliases
+alias avr-objdump="avr-objdump --no-show-raw-insn"
 alias objdump="llvm-objdump --x86-asm-syntax=intel"
 alias readelf="llvm-readelf"
 alias lldb="lldb"
