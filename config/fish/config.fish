7e912509b3f3a3ea5686c0cf8a44077c6c8e3db7
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-07-27 22:17:02 +0200

    Add 'tree' alias for exa
    Also group all ls-related aliases together

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,18 +2,22 @@ fish_vi_key_bindings
 set TERM "xterm-256color"
 set fish_greeting
 
-# Misc aliases
-alias diff="diff --color=auto"
-alias df="df -h"
-alias ls="exa"
-alias ll="exa -l"
-alias egrep="egrep --color=auto"
-alias free="free -h"
-alias py=python3
-alias vim="nvim -p"
+# ls-related aliases
+alias ls="exa --group-directories-first"
+alias la="exa --group-directories-first -la"
+alias ll="exa --group-directories-first -l"
+alias tree="exa --group-directories-first --tree"
 
 # Compiler/binutils aliases
 alias avr-objdump="avr-objdump --no-show-raw-insn"
 alias objdump="llvm-objdump --x86-asm-syntax=intel"
 alias readelf="llvm-readelf"
 alias lldb="lldb"
+
+# Misc aliases
+alias diff="diff --color=auto"
+alias df="df -h"
+alias egrep="egrep --color=auto"
+alias free="free -h"
+alias py=python3
+alias vim="nvim -p"
