aa7cfaa102367a0e62be601c09ddfb2f8d7f3e20
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-10-01 17:02:20 +0200

    fish: detect containers and adjust $PS1 accordingly
    Mainly useful for distrobox.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -4,17 +4,12 @@
 # Set the default prompt command.
 
 function fish_prompt --description "Write out the prompt"
-	# Just calculate this once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
-
 	set -U fish_prompt_pwd_dir_length 0
 	set -l user "\033[1;38;5;77m$USER\033[00m"
-	set -l host "\033[1;38;5;220m$__fish_prompt_hostname\033[00m"
+	set -l host "\033[1;38;5;220m$hostname\033[00m"
+	set -l container "\033[1;38;5;208m$CONTAINER_ID\033[00m"
 	set -l root "\033[31mroot\033[00m"
-	set -l pwd (prompt_pwd)
-	set -l pwd "\033[1;38;5;39m$pwd\033[00m"
+	set -l pwd "\033[1;38;5;39m$(prompt_pwd)\033[00m"
 	set -l prefix ""
 
 	switch $USER
@@ -23,31 +18,23 @@ function fish_prompt --description "Write out the prompt"
 	end
 
 	if set -q SSH_TTY
-		switch $USER
-			case 'ayke'
-				set prefix "$host"
-			case '*'
-				set prefix "$user@$host"
-		end
-	else
-		switch $USER
-			case 'ayke'
-				set prefix ""
-			case '*'
-				set prefix "$user"
+		# TODO: add container ID with different color
+		set prefix "$host"
+	else if set -q CONTAINER_ID
+		set prefix "$container"
+	end
+
+	# add username if needed
+	if [ "$USER" != "ayke" ]
+		if [ "$prefix" = "" ]
+			set prefix "$user"
+		else
+			set prefix "$user@$prefix"
 		end
 	end
 
 	# Like prompt_pwd, but without shortening the parent directories.
 	echo -en "\n$prefix$pwd"
-	#switch $PWD
-	#case "/home/$USER"
-	#	echo -n "~"
-	#case "/home/$USER/*"
-	#	echo -n "~...$PWD"
-	#case '*'
-	#	echo -n "$PWD"
-	#end
 
 	if test "$fish_key_bindings" = "fish_vi_key_bindings"
 		switch $fish_bind_mode
