db0fe7df901d2a146adb1121a89736b395a5ab93
Ayke van Laethem <aykevanlaethem@gmail.com>
2026-01-17 11:28:08 +0000

    Add nix server configuration (for my VPS)

---
+++ nix/server.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }: {
+  # All installed packages
+  environment.systemPackages = with pkgs; [
+    bat
+    byobu
+    delta
+    eza
+    fish
+    git
+    htop
+    hugo
+    neovim
+    optipng
+    pwgen
+    ripgrep
+    tig
+    tmux
+    wget
+  ];
+
+  # Automatic updates
+  system.autoUpgrade = {
+    enable = true;
+    flags = [
+      "--print-build-logs"
+    ];
+    dates = "04:00";
+    randomizedDelaySec = "45min";
+    allowReboot = true;
+  };
+
+  # User management
+  programs.fish.enable = true;
+  users.users.maaike = {
+    isNormalUser = true;
+    home = "/home/maaike";
+    shell = pkgs.fish;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEkqGExf0OKNa+Fzq09JRYQjelMK3CjkqUiQEtrnZAry ayke@elstar-linux'' ];
+  };
+
+  # Firewall
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # Web service
+  services.nginx = {
+    enable = true;
+    virtualHosts."ruby.aykevl.nl" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "/srv/aykevl.nl";
+      locations."/".extraConfig = ''
+        try_files /blog/$uri /blog/$uri/index.html $uri $uri/ =404;
+      '';
+    };
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "acme@aykevl.nl";
+  };
+}
